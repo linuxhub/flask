da46eead9bbbc5eabf770cf9d2df9c591efde6c7
zepinglai <zepinglai@gmail.com>
2015-03-27 01:02:15 +0800

    自定义错误页面

---
+++ 03.第三章.模板/3.3自定义错误页面/hello.py
@@ -0,0 +1,27 @@
+from flask import  Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html',name=name)
+
+#404 Error page
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+#500 Error page
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'), 500
+
+if __name__ == '__main__':
+	app.run(debug=True)
