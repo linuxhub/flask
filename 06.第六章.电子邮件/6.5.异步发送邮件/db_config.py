9b9e147854aed13ed42ffe9de2bef35deebbd67e
zeping <zepinglai@gmail.com>
2015-05-06 16:33:31 +0800

    6.5.异步发送邮件

---
+++ 06.第六章.电子邮件/6.5.异步发送邮件/db_config.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#encoding:utf8
+#author: linuxhub.org
+# pip install flask-sqlalchemy
+
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
+#配置数据库
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite') #当前目录下的data.sqlite
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+#定义Role模型
+class Role(db.Model):
+              __tablename__ = 'roles'                       #表名
+              id = db.Column(db.Integer, primary_key=True)  #列名
+              name = db.Column(db.String(64), unique=True)  #列名
+              #users = db.relationship('User', backref='role') 
+              users = db.relationship('User', backref='role', lazy='dynamic') # dynamic禁止自动执行查询
+
+              def __repr__(self):
+                            return '<Role %r>' % self.name
+
+#定义User模型
+class User(db.Model):
+              __tablename__ = 'users'                                       #表名
+              id = db.Column(db.Integer, primary_key=True)                  #列名
+              username = db.Column(db.String(64), unique=True, index=True)  #列名
+              role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+              def __repr__(self):
+                            return '<Role %r>' % self.username
+
---
+++ 06.第六章.电子邮件/6.5.异步发送邮件/hello.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+#encoding:utf8
+#author: linuxhub.org
+
+from flask import  Flask, render_template, session 
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask import session
+from flask import redirect, url_for 
+
+from db_config import db
+from db_config import Role,User
+
+
+from threading import Thread  #双线程
+
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'linuhub'
+class NameForm(Form):
+              name = StringField('What is you name? ', validators=[Required()])
+              submit = SubmitField('Submit')
+
+
+#发信邮箱配置信息
+app.config['MAIL_SERVER'] = 'smtp.163.com'   #163邮件smtp
+app.config['MAIL_PORT'] = 465                #163邮件端口
+app.config['MAIL_USE_TLS'] = False           #TLS协议
+app.config['MAIL_USE_SSL'] = True            #SSL协议(启用)
+app.config['MAIL_USERNAME'] = 'zepingmon'    # 163邮件 用户名
+app.config['MAIL_PASSWORD'] = '123456789'    # 163邮件 密码
+#app.config['ADMINS'] = 'zepingmon@163.com'  # 发件人
+
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'                  #主题前缀
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <zepingmon@163.com>'  #发件人
+app.config['FLASKY_ADMIN'] = 'root@linuxhub.org'                       #收件人
+
+
+#初始化Mail对象连接smtp服务器
+from flask.ext.mail import Mail
+from flask.ext.mail import Message
+mail = Mail(app)
+
+
+def send_async_email(app, msg):
+              with app.app_context():
+                            mail.send(msg)
+
+
+#邮件
+def send_email(to, subject, template, **kwargs):
+              
+              '''  邮件发送  '''
+
+              # 收件人, 主题,  渲染邮件正文的模板, 关键字参数列表
+              msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                            sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+
+              msg.body = render_template(template + '.txt', **kwargs)    #邮件内容.主题
+              msg.html = render_template(template + '.html', **kwargs)   #邮件内容.内容
+
+              thr = Thread(target=send_async_email, args=[app, msg])
+              thr.start()
+              return thr
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+             
+              form = NameForm()
+              if form.validate_on_submit():
+                            user = User.query.filter_by(username=form.name.data).first()
+                            if user is None:   
+                                          user = User(username = form.name.data)
+                                          db.session.add(user)  
+                                          session['known'] = False
+                                          
+                                          #每当表单接收新名字时,程序都会给管理员发送一封电子邮件
+                                          if app.config['FLASKY_ADMIN']:
+                                                        
+                                                        send_email(app.config['FLASKY_ADMIN'], 'New User','mail/new_user', user=user)   
+                                                        #'mail/new_user'的意思是这个目录下: templates/mail/new_user[.txt|.html] 文件
+                                                                                                  
+                            else:   
+                                          session['known'] = True
+                                          
+                            session['name'] = form.name.data
+                            form.name.data = ''
+                            return redirect(url_for('index'))
+              
+              return render_template('index.html', 
+                                     form = form,
+                                     name = session.get('name'), 
+                                     known = session.get('known', False))
+
+if __name__ == '__main__':
+              app.run(debug=True, port=80)
+             
