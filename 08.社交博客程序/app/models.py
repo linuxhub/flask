6cb8480718989f42ff9710aeabbf277ec078ba74
zeping <zepinglai@gmail.com>
2015-05-15 11:40:13 +0800

    11.3.1.创建虚拟博客文章数据 11-7.开发所需的依赖文件 11-8.生成虚拟用户和博客文章

--- 08.社交博客程序/app/models.py
+++ 08.社交博客程序/app/models.py
@@ -229,6 +229,35 @@ class User(UserMixin, db.Model):
                             
                             return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url, hash=hash, size=size, default=default, rating=rating)
                             #例: URL结果: http://www.gravatar.com/avatar/572bc94368fec49fab782b6cda3a3597?s=100&d=identicon&r=g
+               
+               
+               
+              #生成虚拟用户（在做分页功能的时候需要大量的测试数据）
+              ''' 使用: python manage.py shell
+                       User.generate_fake(100)
+              '''
+              @staticmethod
+              def generate_fake(count=100):
+                            from sqlalchemy.exc import IntegrityError
+                            from random import seed
+                            import forgery_py
+
+                            seed()
+                            for i in range(count):
+                                          u = User(email=forgery_py.internet.email_address(),
+                                                   username=forgery_py.internet.user_name(True),
+                                                   password=forgery_py.lorem_ipsum.word(),
+                                                   confirmed=True,
+                                                   name=forgery_py.name.full_name(),
+                                                   location=forgery_py.address.city(),
+                                                   about_me=forgery_py.lorem_ipsum.sentence(),
+                                                   member_since=forgery_py.date.date(True))
+                                          db.session.add(u)
+                                          try:
+                                                        db.session.commit()
+                                          except IntegrityError:
+                                                        db.session.rollback()
+                              
                                     
               def __repr__(self):
                             return '<Role %r>' % self.username
@@ -264,3 +293,24 @@ class Post(db.Model):
               body = db.Column(db.Text)                     #列名:  文章内容                         
               timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow) #列名：时间戳
               author_id = db.Column(db.Integer, db.ForeignKey('users.id'))     #作者id 链接到users表的id
+              
+              #生成虚拟博客文章 （在做分页功能的时候需要大量的测试数据）
+              ''' 使用: python manage.py shell
+                        Post.generate_fake(100)
+              '''              
+              @staticmethod
+              def generate_fake(count=100):
+                            from random import seed, randint
+                            import forgery_py
+
+                            seed()
+                            user_count = User.query.count()
+                            for i in range(count):
+                                          u = User.query.offset(randint(0, user_count - 1)).first()
+                                          p = Post(body=forgery_py.lorem_ipsum.sentences(randint(1, 5)),
+                                                   timestamp=forgery_py.date.date(True),
+                                                   author=u)
+                                          db.session.add(p)
+                                          db.session.commit()
+              
+              
