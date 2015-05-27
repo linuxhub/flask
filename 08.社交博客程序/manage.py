75d5e31b05a56f65008c161e3fa4458d3296afe2
zeping <zepinglai@gmail.com>
2015-05-27 16:30:13 +0800

    15.1.获取代码覆盖报告[测试]

--- 08.社交博客程序/manage.py
+++ 08.社交博客程序/manage.py
@@ -4,6 +4,15 @@
 '''  启动脚本  '''
 
 import os
+
+#代码覆盖检测所需的
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
+
 from app import create_app, db
 from app.models import User, Role, Permission, Post, Comment
 from flask.ext.script import Manager, Shell  
@@ -23,11 +32,31 @@ manager.add_command('db', MigrateCommand)
 
 #启动单元测试命令
 @manager.command
-def test():
+def test(coverage=False):
     ''' Run the unit tests '''
+    
+    #代码覆盖检测所需的
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+        
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    
+    #代码覆盖检测
+    ''' 获取代码覆盖报告:  python manage.py test --coverage   '''
+    if COV:
+        COV.stop()
+        COV.save()
+        print(u'报告摘要:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print(u'详细报告文件: file://%s/index.html' % covdir)
+        COV.erase()    
 
 
 
